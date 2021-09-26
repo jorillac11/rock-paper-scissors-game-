9ac0c626268cc83c288b27923212228901729905
jorillac11 <79588778+jorillac11@users.noreply.github.com>
2021-09-26 18:09:51 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,9 +4,9 @@ import random
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # invokes / uses the function we got from the third-party package. this one happens to read env vars from the ".env" file. see the "python-dotenv" package docs for more info
 
-PLAYER_NAME = os.getenv("PLAYER_NAME", default="Player One")
+PLAYER_NAME = os.getenv("PLAYER_NAME", default="Player One") # uses the os module to read the specified environment variable and store it in a corresponding python variable
 
 print (f"Welcome '{PLAYER_NAME}' this is my rock, paper, scissors game!")
 
@@ -35,13 +35,13 @@ print(computer_choice)
 if(user_choice == "rock" and computer_choice == "scissors"):
     print("Congratulations,you win!")
 elif(user_choice == "rock" and computer_choice == "paper"):
-    print("Oh, the computer won. It's ok.")
+    print("Oh, the computer won. It's ok, try again.")
 elif(user_choice == "paper" and computer_choice == "scissors"):
-    print("Oh, the computer won. It's ok.")
+    print("Oh, the computer won. It's ok, try again.")
 elif(user_choice == "paper" and computer_choice == "rock"):
     print("Congratulations,you win!")
 elif(user_choice == "scissors" and computer_choice == "rock"):
-    print("Oh, the computer won. It's ok.")
+    print("Oh, the computer won. It's ok, try again.")
 elif(user_choice == "scissors" and computer_choice == "paper"):
     print("Congratulations,you win!")
 else: print("It's a tie!")
